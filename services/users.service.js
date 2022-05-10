9be3c344152e1f644acddb2202d1f52ba3078c82
venugopal <venu.shastri@gmail.com>
2022-05-10 05:45:41 +0530

    users commit

---
+++ services/users.service.js
@@ -0,0 +1,15 @@
+module.exports = {
+    getAllUsers: (type) => {
+        let headers = {
+            "content-type": "application/json"
+        };
+        return new Promise((resolve, reject) => {
+
+                const body={symbol:type,price:1245};
+                
+                    resolve(body);
+               
+        })
+
+    },
+};
