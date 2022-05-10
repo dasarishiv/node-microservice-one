04ffb4447eb09e623d88476eca5f1c2a3b4dbda5
venu-shastri <venu.shastri@gmail.com>
2022-05-10 07:24:21 +0530

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 const routes = require('./routes/index');
 const app = express();
 
--- services/users.service.js
+++ services/users.service.js
@@ -1,13 +1,39 @@
+const { response } = require('express');
+const { all } = require('../routes');
+
+var faunadb = require('faunadb'),
+q = faunadb.query
 module.exports = {
-    getAllUsers: (type) => {
+    getAllUsers:  (type) => {
         let headers = {
             "content-type": "application/json"
         };
-        return new Promise((resolve, reject) => {
+        return new Promise( async (resolve, reject) => {
 
-                const body={symbol:type,price:1245};
+            var client = new faunadb.Client({ secret:'fnAElzr85hAASaX-JuUc2Y1MRtwKO6OXu07W5hQd' ,domain: "db.us.fauna.com"})
+    
+           
+           
+            let allDocuments = await client.query(
+                q.Map(
+                  q.Paginate(q.Documents(q.Collection('users'))),
+                  q.Lambda((user) => q.Get(user)
+                )
+              )
+            ) ;
+            let filteredDocumnets=[];             
+            allDocuments.data.forEach(element => {
+                filteredDocumnets.push(element.data);
+            });
+
+           
+            resolve(filteredDocumnets);
+            
                 
-                    resolve(body);
+             
+            
+                
+                    
                
         })
 
